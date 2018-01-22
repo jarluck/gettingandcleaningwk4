cfb0430d40776fc1ac08fdd023a91afc028a5fd2
jarluck <jarluck@gmail.com>
2018-01-22 00:28:18 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gettingandcleaningwk4
-Project to aggregate and tidy accelerometer data from the UCI Wearable Computing project 
+# Data Analysis and Tidying Project
+
+This code aggregates and tidies accelerometer data from the UCI Wearable Computing project.
+
+The raw data is at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ; we do not include it here as the margins of this GitHub are too small to contain it. The script assumes that it is being run from the root directory of the file.
+
+The script inputs and combines the testing and training data, and labels each measurement following the descriptions in features.txt. It also labels the broad category of behavior the measurement was observed in (e.g, walking, standing), and labels the test subject from whom the data was recorded. We only keep those measurements that are of either the mean or the standard deviation of some variable. 
+
+
+The outputted tidy data set (TidyMotionData.txt) has each combination of  test subject and activity category, and lists the average of each measurement accordingly. 
+
---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+#read in test and train data
+testdata <- read.table("test/X_test.txt")
+subject_test <- read.table("test/subject_test.txt")
+y_test<- read.table("test/y_test.txt")
+
+traindata <- read.table("train/X_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+y_train<- read.table("train/y_train.txt")
+
+#merge test and train
+data <-rbind(testdata, traindata)
+subject <- rbind(subject_test, subject_train)
+y <- rbind(y_test,y_train)
+
+#read in labels
+actlab <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
+#add measurement labels. 
+names(data) <- features$V2
+
+#only keep those measurements that are of a mean or standard deviation
+data <- data[,grepl("(std)|(mean)", names(data))]
+
+#label subjects and activities
+data$subject <- subject$V1
+data$activity <- actlab$V2[y$V1]
+#put subject and activity as 1st and 2nd cols
+data <- data[,c(ncol(data), ncol(data) - 1, 1:ncol(data) - 2)]
+
+#make new tidy data frame, which groups by activity and test subject
+tidyframe <- data.frame(matrix(ncol = ncol(data), nrow = 0))
+names(tidyframe) <- names(data)
+
+
+act <- unique(data$activity)
+sub <- unique(data$subject)
+for (i in 1:length(act))
+{
+  for (j in 1:length(sub))
+  {
+    newrow <- c(act[i], sub[j], 
+                colMeans(data[data$activity == act[i]&data$subject == sub[j],3:ncol(data)]))
+    tidyframe <- rbind(tidyframe, newrow)
+  }
+}
+#output result
+write.table(tidyframe, "tidyMotionData.txt", sep="\t", row.name = FALSE)
